648f2915972fd1cf1c3e5ef257f6ecc996f129a6
Gregorovi2703 <chaychinhendri27@gmail.com>
2026-02-04 23:24:02 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000105999_chaychinhendri/week01/GameStore.kt
+++ src/oop_00000105999_chaychinhendri/week01/GameStore.kt
@@ -3,4 +3,8 @@ package oop_00000105999_chaychinhendri.week01
 fun main() {
     val gameTitle: String = "Split Fiction"
     val price: Int = 725000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) price - (price * 20/100) else price - (price * 10/100)
