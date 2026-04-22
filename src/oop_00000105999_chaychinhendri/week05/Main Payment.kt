c24ddb4a71e21ee2c9f42588c3d435a20e41a194
Gregorovi2703 <chaychinhendri27@gmail.com>
2026-04-22 11:50:55 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000105999_chaychinhendri/week05/Main Payment.kt
@@ -0,0 +1,15 @@
+package oop_00000105999_chaychinhendri.week05
+
+fun main() {
+    println("=== PAYMENT METHOD TEST ===")
+
+    val ew = EWallet("Gregor", 50000.0)
+    val cc = CreditCard("Dwiky", 100000.0)
+
+    val payments: List<PaymentMethod> = listOf(ew, cc)
+
+    for (method in payments) {
+        println("\n${method.accountName} mencoba bayar 75000")
+        method.processPayment(75000.0)
+    }
+}
